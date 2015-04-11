0b587032039c0fc5279171444cd8fb5f5056858f
Matt Carrier <icecreammatt@gmail.com>
2015-04-11 00:55:37 -0700

    Added callback object to send data back to dockerhub

--- handler.go
+++ handler.go
@@ -2,11 +2,27 @@ package listener
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os/exec"
 )
+
+type CallbackState string
+
+const (
+	Success CallbackState = "success"
+	Failure CallbackState = "failure"
+	Error   CallbackState = "error"
+)
+
+type CallbackPayload struct {
+	State       CallbackState `json:"state"`
+	Description string        `json:"description"`
+	Context     string        `json:"context"`
+	TargetUrl   string        `json:"target_url"`
+}
 
 type Handler interface {
 	Call(HubMessage)
@@ -37,16 +53,35 @@ func reloadHandler(msg HubMessage) {
 	fmt.Println("Reload handler called")
 	log.Println("received message to reload ...")
 	log.Printf("Callback URL: %s\n", msg.CallbackUrl)
-	if msg.CallbackUrl != "" {
-		jsonStr := []byte(`{"state":"success"}`)
-		http.Post(msg.CallbackUrl, "application/json", bytes.NewBuffer(jsonStr))
-		log.Println("calling callback URL:", msg.CallbackUrl)
-	}
 	out, err := exec.Command("./reload.sh", msg.Repository.RepoName).Output()
+
+	var callbackState CallbackState
+	callbackPayload := CallbackPayload{
+		State: callbackState,
+		//TODO: Limit description to 255 characters
+		Description: string(out),
+		Context:     "Webhook Listener",
+		TargetUrl:   "http://ci.acme.com/results/afd339c1c3d27",
+	}
 	if err != nil {
 		log.Println("ERROR EXECUTING COMMAND IN RELOAD HANDLER!!")
 		log.Println(err)
-		return
+		callbackPayload.State = Error
+		callbackPayload.Description = err.Error()
+	} else {
+		callbackPayload.State = Success
+	}
+
+	if msg.CallbackUrl != "" {
+		res, err := json.Marshal(callbackPayload)
+		if err != nil {
+			fmt.Println("error marshaling callbackPayload")
+		}
+		_, err = http.Post(msg.CallbackUrl, "application/json", bytes.NewBuffer(res))
+		log.Println("calling callback URL:", msg.CallbackUrl)
+		if err != nil {
+			fmt.Println("Error posting to callbackUrl", err.Error())
+		}
 	}
 	log.Println("output of reload.sh is", string(out))
 }
